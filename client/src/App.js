a40b8de5c4dac07e5eb0b165e3392b1f783b8581
cantstoptheunk <ryn10sng@gmail.com>
2020-06-18 21:39:42 -0600

    Changed hookrouter to react router

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import ChatPage from './components/ChatPage'
 import NotFoundPage from './components/NotFoundPage'
 import { useRoutes } from "hookrouter";
 import { ParallaxProvider } from 'react-scroll-parallax'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 const routes = {
   "/": () => <HomePage />,
@@ -12,12 +13,13 @@ const routes = {
 };
 
 export default function App() {
-  const routeResult = useRoutes(routes);
-
   return (
-    <ParallaxProvider>
-      {routeResult || <NotFoundPage />}
-    </ParallaxProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="about/*" element={<ChatPage />} />
+      </Routes>
+    </BrowserRouter>
   )
-
 }
+  
