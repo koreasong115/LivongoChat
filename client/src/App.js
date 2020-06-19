200ce0b4bad8e93af4e04bb918fde1f197e4ebcb
cantstoptheunk <ryn10sng@gmail.com>
2020-06-18 22:39:45 -0600

    Testing package.json changes to react router

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,19 @@ import ChatPage from './components/ChatPage'
 import NotFoundPage from './components/NotFoundPage'
 import { useRoutes } from "hookrouter";
 import { ParallaxProvider } from 'react-scroll-parallax'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-const routes = {
-  "/": () => <HomePage />,
-  "/home": () => <HomePage />,
-  "/about": () => <ChatPage />,
-};
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="about/*" element={<ChatPage />} />
-      </Routes>
+
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/home" component={HomePage} />
+        <Route exact path="/about" component={ChatPage} />
+
+      </Switch>
     </BrowserRouter>
   )
 }
-  
